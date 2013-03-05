d25bd01a81f9e4cb5fcbcfb0e6ddea5b123eabbd
Juho Vepsalainen <bebraw@gmail.com>
2013-03-05 09:15:37 +0200

    Set up Magellan initially

--- index.md
+++ index.md
@@ -1,5 +1,6 @@
 ---
 layout: default
+js: index
 ---
 
 # About
---
+++ js/index.js
@@ -0,0 +1,10 @@
+require(['jquery', './foundation/foundation', './foundation/foundation.magellan'], function($) {
+    $(document).foundation();
+
+    // TODO: modernizr
+    // TODO: zepto (load conditionally)
+    // TODO: construct TOC
+    // TODO: smooth transitions
+
+    // TODO: twitter + rss
+});
